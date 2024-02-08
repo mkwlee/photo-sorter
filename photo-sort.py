f491d70833d9d78bdc24e643d5b034cbf173eb95
Matthew <mkwlee89@gmail.com>
2024-02-07 17:38:12 -0800

    sort valid photos from argument-directory into output/yyyy/mmm folders

--- photo-sort.py
+++ photo-sort.py
@@ -1,1 +1,18 @@
-print('Hello World')
+import os
+import sys
+from PIL import Image
+from helper import months_dict
+
+listFiles = os.listdir(sys.argv[1])
+
+for file in listFiles:
+    image = Image.open(sys.argv[1]+'/'+file)
+    dictExif = image.getexif()
+    if 306 in dictExif:
+        listDate = (dictExif[306][:7]).split(':')
+        strDateDir = listDate[0]+'/'+months_dict[listDate[1]]+'/'
+        print(file, strDateDir)
+        if not os.path.exists('output/'+strDateDir):
+            os.makedirs('output/'+strDateDir)
+        image.save('output/'+strDateDir+file)
+
