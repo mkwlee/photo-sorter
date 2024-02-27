d1e39c64646d643b5cbd645f6ee97ce1b9b95ea2
Matthew <mkwlee89@gmail.com>
2024-02-26 20:00:01 -0800

    added video sorting ability added context of sorting files of photos added undated method

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+import math
 months_dict = {
     '01': 'Jan',
     '02': 'Feb',
@@ -12,3 +13,22 @@ months_dict = {
     '11': 'Nov',
     '12': 'Dec'
 }
+
+def get_progress_bar (current, total):
+    bar = ''
+    percent = (current / float(total))*100
+    dots = math.floor(50 * (current / float(total)))
+    for x in range(0, dots):
+        bar += '\u2588'
+    for x in range(0, 50 - dots):
+        bar += '\u2591'
+    return bar+' - '+str(round(percent))+'%'
+
+def count_total_elements(lst):
+    total_elements = 0
+    for item in lst:
+        if isinstance(item, list):
+            total_elements += count_total_elements(item)
+        else:
+            total_elements += 1
+    return total_elements
--- photo-sort.py
+++ photo-sort.py
@@ -1,18 +1,43 @@
-import os
-import sys
-from PIL import Image
-from helper import months_dict
+from PIL import Image, ImageFilter
+from pillow_heif import HeifImagePlugin
+import imageio.v3 as imageio
+import os, sys
+import shutil
+from helper import months_dict, get_progress_bar, count_total_elements
 
-listFiles = os.listdir(sys.argv[1])
-
-for file in listFiles:
-    image = Image.open(sys.argv[1]+'/'+file)
-    dictExif = image.getexif()
-    if 306 in dictExif:
-        listDate = (dictExif[306][:7]).split(':')
-        strDateDir = listDate[0]+'/'+months_dict[listDate[1]]+'/'
-        print(file, strDateDir)
-        if not os.path.exists('output/'+strDateDir):
-            os.makedirs('output/'+strDateDir)
-        image.save('output/'+strDateDir+file)
-
+baseDir = os.listdir(sys.argv[1])
+listFolders = []
+for x in baseDir:
+    listFolders.append(os.listdir(sys.argv[1]+'/'+x))
+# listFiles = os.listdir(sys.argv[1])
+totalFiles = count_total_elements(listFolders)
+print('Sorting...\n')
+count = 1
+for index, folder in enumerate(listFolders):
+    for file in folder:
+        print("\033[A                             \033[A")
+        print(str(count) + '/' + str(totalFiles), get_progress_bar(count, totalFiles))
+        count+=1
+        # print(sys.argv[1]+baseDir[index]+'/'+file)
+        try:
+            metadata = imageio.immeta(sys.argv[1]+'/'+baseDir[index]+'/'+file)
+            possible_date_keys = ['datetime', 'creation_time', 'date', 'timestamp', 'DateTime']
+            correct_key = None
+            for key in possible_date_keys:
+                if key in metadata:
+                    correct_key = key
+                    break
+            if correct_key is not None:
+                listDate = (metadata[correct_key][:7]).split(':')
+                if len(listDate) == 1:
+                    listDate = listDate[0].split('-')
+                strDateDir = listDate[0] + '/' + listDate[1] + months_dict[listDate[1]] + '/'
+                if not os.path.exists('output/' + strDateDir):
+                    os.makedirs('output/'+strDateDir)
+                shutil.move(sys.argv[1]+'/'+baseDir[index]+'/'+file, 'output/'+strDateDir+file)
+            else:
+                if not os.path.exists('output/undated'):
+                    os.makedirs('output/undated')
+                shutil.move(sys.argv[1]+'/'+baseDir[index]+'/'+file, 'output/undated/'+file)
+        except Exception as e:
+            pass
